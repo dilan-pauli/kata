623bb9d11660e9e8a843c7d469194cfda7aa194d
Dilan Pauli <dilan.pauli@morneaushepell.com>
2023-02-22 09:02:24 -0600

    First Commit

---
+++ golang/hello-world/hello.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("hello world")
+
+    fmt.Println("go" + "lang")
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+
+
+    // https://gobyexample.com/variables
+    var a = "inital"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b,c)
+
+
+
+
+}
+
